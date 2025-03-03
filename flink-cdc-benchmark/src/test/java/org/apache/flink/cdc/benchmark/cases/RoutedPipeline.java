55db13853416e80da20fbfb6910edcb0d2f323de
yuxiqian <34335406+yuxiqian@users.noreply.github.com>
2025-03-03 17:24:55 +0800

    Fix code style

--- flink-cdc-benchmark/src/test/java/org/apache/flink/cdc/benchmark/cases/RoutedPipeline.java
+++ flink-cdc-benchmark/src/test/java/org/apache/flink/cdc/benchmark/cases/RoutedPipeline.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.flink.cdc.benchmark.cases;
 
 import org.apache.flink.cdc.benchmark.common.PipelineBenchmarkBase;
@@ -13,13 +30,13 @@ import org.openjdk.jmh.infra.Blackhole;
 import java.util.Arrays;
 import java.util.Collections;
 
+/** Benchmarking test cases with routed pipelines. */
 public class RoutedPipeline extends PipelineBenchmarkBase {
 
     @Benchmark
     public void runRoutedPipelineBenchmarkTest(Blackhole blackhole) throws Exception {
         PipelineBenchmarkOptions options =
-                new PipelineBenchmarkOptions.Builder()
-                        .setParallelism(1).build();
+                new PipelineBenchmarkOptions.Builder().setParallelism(1).build();
         Arrays.stream(
                         runInPipeline(
                                 Collections.singletonList(
--- flink-cdc-benchmark/src/test/java/org/apache/flink/cdc/benchmark/common/PipelineBenchmarkOptionsBuilder.java
+++
@@ -1,1 +0,0 @@
-package org.apache.flink.cdc.benchmark.common;
